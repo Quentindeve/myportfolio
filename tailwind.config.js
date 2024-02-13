27ee05854825625da3671856bc2c0ac9dca676da
Quentindeve <quentindutilleul@pm.me>
2024-02-13 16:32:26 +0100

    Tailwind config + custom styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "templates/**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cyberblue': '#00dcdf',
+        'cyberyellow': '#fcee0a'
+      },
+    }
+  },
+  plugins: [],
+}
